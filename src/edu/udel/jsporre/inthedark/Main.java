6ba2259db0f73c782fbcabfa5dd35a23e07fbc49
jsporre <jsporre@udel.edu>
2014-10-06 19:08:33 -0400

    Added
    Player actions

--- src/edu/udel/jsporre/inthedark/Main.java
+++ src/edu/udel/jsporre/inthedark/Main.java
@@ -2,8 +2,12 @@ package edu.udel.jsporre.inthedark;
 
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("hello world!");
-  }
+    public static void main(String[] args) {
+	System.out.println("hello world!");
+    }
+
+    public String toString() {
+
+    }
 
 }
---
+++ src/edu/udel/jsporre/inthedark/game/PlayerChangeDirection.java
@@ -0,0 +1,5 @@
+package edu.udel.jsporre.inthedark.game;
+
+public class PlayerChangeDirection {
+
+}
