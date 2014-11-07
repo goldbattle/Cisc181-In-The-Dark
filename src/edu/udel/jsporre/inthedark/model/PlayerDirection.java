0c42a4ec4c76b7d52ce80dacdf5cc4067b905c46
goldbattle <patfgeneva@gmail.com>
2014-11-07 12:02:23 -0500

    Clean - Everything

--- src/edu/udel/jsporre/inthedark/model/PlayerDirection.java
+++
@@ -1,8 +0,0 @@
-package edu.udel.jsporre.inthedark.model;
-
-public enum PlayerDirection {
-    DIRECTION_UP,
-    DIRECTION_DOWN,
-    DIRECTION_RIGHT,
-    DIRECTION_LEFT
-}
--- src/edu/udel/jsporre/inthedark/util/CountdownTimer.java
+++
@@ -1,5 +0,0 @@
-package edu.udel.jsporre.inthedark.util;
-
-public class CountdownTimer {
-
-}
