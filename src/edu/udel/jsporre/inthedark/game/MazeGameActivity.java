d792862461b1f96cf0c25dd5a96a1f0cf14cd44f
goldbattle <patfgeneva@gmail.com>
2014-11-27 21:57:50 -0500

    Formating

--- src/edu/udel/jsporre/inthedark/game/MazeGameActivity.java
+++ src/edu/udel/jsporre/inthedark/game/MazeGameActivity.java
@@ -19,83 +19,84 @@ public class MazeGameActivity extends Activity implements GameListener<MazeGame>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        status = new TextView(this);
-        status.setTypeface(Typeface.MONOSPACE);
-        timer = new TextView(this);
-        timer.setTypeface(Typeface.MONOSPACE);
-        //gameView= new TicTacToe5x5View2D(this);
-        gameView = new MazeGameView(this);
-        status.setTypeface(Typeface.MONOSPACE);
+	super.onCreate(savedInstanceState);
+	status = new TextView(this);
+	status.setTypeface(Typeface.MONOSPACE);
+	timer = new TextView(this);
+	timer.setTypeface(Typeface.MONOSPACE);
+	//gameView= new TicTacToe5x5View2D(this);
+	gameView = new MazeGameView(this);
+	status.setTypeface(Typeface.MONOSPACE);
 
-        
-        
-        // So much crap to make this stay on the right
-        RelativeLayout relativeLayout = new RelativeLayout(this);
-        RelativeLayout.LayoutParams relativeLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        relativeLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        relativeLayout.addView(timer, relativeLayoutParams);
-        
-        
-        // Nice layout, status
-        LinearLayout ll0 = new LinearLayout(this);
-        ll0.addView(status);
-        ll0.addView(relativeLayout);
-        ll0.setOrientation(LinearLayout.HORIZONTAL);
-        
-        // Layout for game
-        LinearLayout ll1 = new LinearLayout(this);
-        ll1.addView(ll0);
-        ll1.addView(gameView);
-        ll1.setOrientation(LinearLayout.VERTICAL);
-        
-        // Start the game, and display
-        startGame();
-        setContentView(ll1);
+
+
+	// So much crap to make this stay on the right
+	RelativeLayout relativeLayout = new RelativeLayout(this);
+	RelativeLayout.LayoutParams relativeLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+	relativeLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+	relativeLayout.addView(timer, relativeLayoutParams);
+
+
+	// Nice layout, status
+	LinearLayout ll0 = new LinearLayout(this);
+	ll0.addView(status);
+	ll0.addView(relativeLayout);
+	ll0.setOrientation(LinearLayout.HORIZONTAL);
+
+	// Layout for game
+	LinearLayout ll1 = new LinearLayout(this);
+	ll1.addView(ll0);
+	ll1.addView(gameView);
+	ll1.setOrientation(LinearLayout.VERTICAL);
+
+	// Start the game, and display
+	startGame();
+	setContentView(ll1);
     }
 
     private void startGame() {
-        game = new MazeGame();
-        
-        game.addGameListener(this);
-        game.addGameListener(new MazeAI(game));
-        game.start(new AndroidTicker());
-        //game.createDefaultGame();
-        //game.addGameListener(new ConsoleListener());
-        game.perform(new ActionCreateMaze(game.ROWS, game.COLUMNS));
+	game = new MazeGame();
+
+	game.addGameListener(this);
+	game.addGameListener(new MazeAI(game));
+	game.start(new AndroidTicker());
+	//game.createDefaultGame();
+	//game.addGameListener(new ConsoleListener());
+	game.perform(new ActionCreateMaze(game.ROWS, game.COLUMNS));
     }
-    
+
     public void updateViews() {
-        status.setText(game.getStatus());
-        timer.setText(game.getTimer());
-        gameView.invalidate();
+	status.setText(game.getStatus());
+	timer.setText(game.getTimer());
+	gameView.invalidate();
     }
 
     @Override
     public void onPerformActionEvent(Action<MazeGame> action, MazeGame game) {
-        updateViews();
+	updateViews();
     }
-    
+
     @Override
     public void onStartEvent(MazeGame game) {
-        updateViews();
+	updateViews();
     }
 
     @Override
     public void onEndEvent(MazeGame game) {
-        updateViews();
+	updateViews();
     }
-    
+
     @Override
     public void onTickEvent(MazeGame game) {
-        updateViews();
+	updateViews();
     }
-    
+
     @Override
     public void onEvent(String event, MazeGame game) {
-        updateViews();
+	updateViews();
     }
-     public MazeGame getCurrentGame(){
-    	 return game;
-     }
+    
+    public MazeGame getCurrentGame(){
+	return game;
+    }
 }
